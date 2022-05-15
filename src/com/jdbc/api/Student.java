d8dc9e1308a2582293da87bd702b3ca0cb353d46
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2022-05-15 23:23:58 +0100

    student model created

---
+++ src/com/jdbc/api/Student.java
@@ -0,0 +1,31 @@
+package com.jdbc.api;
+
+public class Student {
+    private int rollNo;
+    private String studentName;
+    private String studentAddress;
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentAddress() {
+        return studentAddress;
+    }
+
+    public void setStudentAddress(String studentAddress) {
+        this.studentAddress = studentAddress;
+    }
+}
